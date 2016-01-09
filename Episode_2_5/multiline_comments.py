52585de3743e90999822f99d1c8866c321e93acc
Dor-Ron <dor.rondel@gmail.com>
2016-01-09 00:44:07 -0500

    Added files for Episode 2.5

--- Episode_2_5/multiline_comments.py
+++ Episode_2_5/multiline_comments.py
@@ -1,15 +1,21 @@
 print("Shalom")  # xfgcvhjnkl;/
 
+
+
+# הערה רב שורתית מתחילה ב3 מרכאות
 """
 xfregthyhukl
 ewrxtyguiojkp
 rextcyvubiknlkm;
 """
+#ונגמרת ב3 מרכאות
 
+#המרכאות יכולות גם להיות מרכאות אחדות
 '''
 zwerxtfygujilm
 exrctybunml
 rctyubiom
 '''
+#העיקר זה לסגור הערה רב שורתית עם אותו סוג של מרכאות שפתחו את ההערה
 
 print("Lehitraot")
