68ad77397071ea4577cf70114c1458d1657f94e4
Dor Rondel <dor.rondel@gmail.com>
2016-08-24 11:08:16 -0400

    added episode 5 file

---
+++ Episode_5/concatenation.py
@@ -0,0 +1,16 @@
+'''
+אפשר להוסיף ולהכפיל מחרוזות אבל אי אפשר לחסר או לחלק אחת
+'''
+
+print("First String" + " " + "Second String")
+
+print("C++ " * 5)
+
+'''
+יש לנו רציפי בריחה לפתור את הבעייה ההבאה, להתחיל שורה חדשה, ולעשות רווח גדול
+print('Tomer's dog is nice')
+'''
+
+print('Tomer\'s dog is nice')
+print('Top \nBottom')
+print('Left \tRight')
